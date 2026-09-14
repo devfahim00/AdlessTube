78445d8430a845d461b3f5dae9fdfab7233eb260
Fahim <bloggerfahim6@gmail.com>
2026-09-13 20:58:02 -0700

    Enhance VideoItem with uploaderUrl and isLive fields
    Added uploaderUrl and isLive fields to VideoItem. Introduced VideoStreamInfo class for quality selection.

--- lib/models.dart
+++ lib/models.dart
@@ -3,18 +3,22 @@ class VideoItem {
   final String title;
   final String thumbnailUrl;
   final String uploader;
+  final String uploaderUrl;
   final String url;
   final Duration? duration;
   final int? viewCount;
+  final bool isLive;
 
   VideoItem({
     required this.id,
     required this.title,
     required this.thumbnailUrl,
     required this.uploader,
+    this.uploaderUrl = '',
     required this.url,
     this.duration,
     this.viewCount,
+    this.isLive = false,
   });
 
   Map<String, dynamic> toMap() => {
@@ -22,7 +26,9 @@ class VideoItem {
         'title': title,
         'thumbnailUrl': thumbnailUrl,
         'uploader': uploader,
+        'uploaderUrl': uploaderUrl,
         'url': url,
+        'isLive': isLive,
       };
 
   factory VideoItem.fromMap(Map map) => VideoItem(
@@ -30,6 +36,23 @@ class VideoItem {
         title: map['title'] ?? '',
         thumbnailUrl: map['thumbnailUrl'] ?? '',
         uploader: map['uploader'] ?? '',
+        uploaderUrl: map['uploaderUrl'] ?? '',
         url: map['url'] ?? '',
+        isLive: map['isLive'] ?? false,
       );
+}
+
+/// Player এ quality select করার জন্য
+class VideoStreamInfo {
+  final String url;
+  final String quality; // e.g. "720p", "1080p", "480p"
+  final String format; // "muxed" / "video" / "audio"
+  final int? bitrate;
+
+  VideoStreamInfo({
+    required this.url,
+    required this.quality,
+    required this.format,
+    this.bitrate,
+  });
 }
