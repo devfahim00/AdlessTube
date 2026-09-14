e8b636fe5a96438ead19d61fd9f1ecc37b7df42c
devfahim00 <bloggerfahim6@gmail.com>
2026-09-14 11:59:04 -0700

    Integrate PiP callbacks with audio service activity

--- android/app/src/main/kotlin/com/example/adlesstube/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/adlesstube/MainActivity.kt
@@ -1,5 +1,26 @@
 package com.devfahim00.tube
 
-import com.thesparks.android_pip.PipCallbackHelperActivityWrapper
+import android.content.res.Configuration
+import com.ryanheise.audioservice.AudioServiceActivity
+import com.thesparks.android_pip.PipCallbackHelper
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: PipCallbackHelperActivityWrapper()
+class MainActivity : AudioServiceActivity() {
+    private val pipCallbackHelper = PipCallbackHelper()
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        pipCallbackHelper.configureFlutterEngine(flutterEngine)
+    }
+
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean,
+        newConfig: Configuration?
+    ) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        pipCallbackHelper.onPictureInPictureModeChanged(
+            isInPictureInPictureMode,
+            this
+        )
+    }
+}
